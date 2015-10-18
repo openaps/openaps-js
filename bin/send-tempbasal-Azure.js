9c7a194fb33420ad9d0d9aae9852a19067f8f536
amazaheri <alimaz@microsoft.com>
2015-10-18 09:46:59 -0700

    Slight Changes to send-tempbasal-azure.js

--- bin/send-tempbasal-Azure.js
+++ bin/send-tempbasal-Azure.js
@@ -23,9 +23,11 @@ if (!module.parent) {
     var iob_input = process.argv.slice(2, 3).pop()
     var enacted_temps_input = process.argv.slice(3, 4).pop()
     var glucose_input = process.argv.slice(4, 5).pop()
-    var webapi = process.argv.slice(5, 6).pop()
-    if (!iob_input || !enacted_temps_input || !glucose_input || !webapi) {
-        console.log('usage: ', process.argv.slice(0, 2), '<iob.json> <enactedBasal.json> <bgreading.json> <[your_webapi].azurewebsites.net>');
+    var requested_temp_input = process.argv.slice(5, 6).pop()
+    var battery_input = process.argv.slice(6, 7).pop()
+    var webapi = process.argv.slice(7, 8).pop()
+    if (!iob_input || !enacted_temps_input || !glucose_input || !requested_temp_input || !battery_input || !webapi) {
+        console.log('usage: ', process.argv.slice(0, 2), '<iob.json> <enactedBasal.json> <glucose.json> <requestedtemp.json> <battery.json> <[your_webapi].azurewebsites.net>');
         process.exit(1);
     }
 }
@@ -34,20 +36,29 @@ var cwd = process.cwd();
 var glucose_data = require(cwd + '/' + glucose_input);
 var enacted_temps = require(cwd + '/' + enacted_temps_input);
 var iob_data = require(cwd + '/' + iob_input);
+var requested_temp = require(cwd + '/' + requested_temp_input);
+var battery_data = require(cwd +'/' + battery_input);
+
+var data = {
+    bg: glucose_data[0].glucose,
+    iob: iob_data.iob,    
+    tick: requested_temp.tick,
+    eventualBG: requested_temp.eventualBG,
+    snoozeBG: requested_temp.snoozeBG,
+    reason: requested_temp.reason,
+    battery: battery_data.status+" Voltage:"+battery_data.voltage }
 
 
-
-var data = JSON.stringify({
-    "Id": 3,
-    "temp": enacted_temps.temp,
-    "rate": enacted_temps.rate,
-    "duration": enacted_temps.duration,
-    "bg": glucose_data[0].glucose,
-    "iob": iob_data.iob,
-    "timestamp": enacted_temps.timestamp,
-    "received": enacted_temps.recieved
+if (!requested_temp.rate && !requested_temp.duration && enacted_temps.recieved)
+{
+data.temp = enacted_temps.temp;
+data.rate = enacted_temps.rate;
+data.duration = enacted_temps.duration;
+data.timestamp = enacted_temps.timestamp;
+data.received": enacted_temps.recieved;
 }
-);
+
+var payload=JSON.stringify(data);
 
 var options = {
     host: webapi,
@@ -56,7 +67,7 @@ var options = {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json; charset=utf-8',
-        'Content-Length': data.length
+        'Content-Length': payload.length
     }
 };
 
@@ -72,5 +83,5 @@ var req = http.request(options, function (res) {
     });
 });
 
-req.write(data);
+req.write(payload);
 req.end();
